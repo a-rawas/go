90b1e4bfc2be033de4b79f7998f186dd1c53716b
Anand Rawas <speedup.anand@gmail.com>
2024-02-24 17:37:31 -0800

    second draft

---
+++ second.go
@@ -0,0 +1,38 @@
+package main
+
+import "fmt"
+
+
+func main(){
+
+	// printf formating
+
+	const pi float64 = 3.1415
+
+	x:= 33
+
+	isbool := true
+
+	fmt.Printf("Value of pi in float is %f \n", pi)
+
+	fmt.Printf("Type of boolean variable is %T \n", isbool)
+
+	fmt.Printf("%t \n", pi)
+
+	fmt.Printf("base10 formatting for integer %d \n", x)
+
+	fmt.Printf("Type of integer variable is %t \n", x)
+
+	fmt.Printf("Binary representation of integer is %b \n", x)
+
+	fmt.Printf("Char representation of integer is %c \n", x)
+
+	fmt.Printf("Hex representation of integer is %x \n", x)
+
+	var name string = "World is"
+
+	fmt.Println("String length is ", len(name))
+
+	fmt.Println(name + " Great!")
+
+}
